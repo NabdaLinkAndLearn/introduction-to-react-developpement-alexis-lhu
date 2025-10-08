6d4a51ec63ca027d707cc42356f994847d6ec79f
yandai <alexis.lhu@gmail.com>
2025-10-08 16:02:45 +0200

    Initial commit: React project

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+import Logo from './logo.jsx'
+import reactLogo from './assets/react.svg'
+import svg_Test from './assets/svg_test.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div>
+        <Logo href="https://vite.dev" setContent={svg_Test} alt="Vite logo" className="logo" />
+        <Logo href="https://react.dev" setContent={reactLogo} alt="React logo" className="logo" />
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
